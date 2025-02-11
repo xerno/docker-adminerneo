1d0010d280ddc502a6fb2dc8ca2de134d0addece
Zdeněk Kopš <info@xerno.cz>
2025-02-11 13:19:45 +0700

    update to AdminerNeo

--- update.sh
+++ update.sh
@@ -10,11 +10,20 @@ fi
 versions=( "${versions[@]%/}" )
 
 read -r commit_hash fullVersion << EOF
-$(git ls-remote --tags https://github.com/vrana/adminer.git \
+$(git ls-remote --tags https://github.com/adminerneo/adminerneo.git \
 	| awk '{gsub(/refs\/tags\/v/, "", $2); print}' \
 	| sort -rVk2 \
 	| head -1)
 EOF
+
+# Use GNU sed if available (install: brew install gnu-sed), otherwise fallback to sed
+if command -v gsed &>/dev/null; then
+    SED="gsed"
+else
+    SED="sed"
+fi
+
+echo "Using: $SED"
 
 for version in "${versions[@]}"; do
 	if [[ "$fullVersion" != $version* ]]; then
@@ -24,16 +33,17 @@ for version in "${versions[@]}"; do
 	echo "$version: $fullVersion"
 
 	downloadSha256="$(
-		curl -fsSL "https://github.com/vrana/adminer/releases/download/v${fullVersion}/adminer-${fullVersion}.php" \
+		curl -fsSL "https://github.com/adminerneo/adminerneo/releases/download/v${fullVersion}/adminer-${fullVersion}.php" \
 			| sha256sum \
 			| cut -d' ' -f1
 	)"
 	echo "  - adminer-${fullVersion}.php: $downloadSha256"
 
-	sed -ri \
-		-e 's/^(ENV\s+ADMINER_VERSION\s+).*/\1'"$fullVersion"'/' \
-		-e 's/^(ENV\s+ADMINER_DOWNLOAD_SHA256\s+).*/\1'"$downloadSha256"'/' \
-		-e 's/^(ENV\s+ADMINER_COMMIT\s+).*/\1'"$commit_hash"'/' \
-		"$version/fastcgi/Dockerfile" \
-		"$version/Dockerfile"
+    $SED -ri \
+        -e 's/^(ENV\s+ADMINER_VERSION)=.*/\1='"$fullVersion"'/' \
+        -e 's/^(ENV\s+ADMINER_DOWNLOAD_SHA256)=.*/\1='"$downloadSha256"'/' \
+        -e 's/^(ENV\s+ADMINER_COMMIT)=.*/\1='"$commit_hash"'/' \
+        "$version/fastcgi/Dockerfile" \
+        "$version/Dockerfile" || echo "Error modifying files with sed!"
+
 done
